d8e20c251ed53a663d1197148356d50c07f7b307
David Njakai <dvdseroney@gmail.com>
2016-06-20 14:51:00 +0300

    modify procfile

--- hello.py
+++ hello.py
@@ -2,13 +2,13 @@ from flask import Flask
 from prime import primenos
 app = Flask(__name__)
 
-@app.route('/<int:number>', methods=['GET'])
-def primes(number):
-	return str(primenos(number))
-
 @app.route('/')
 def hundred_primes():
 	return str(primenos(100))
+
+@app.route('/<int:number>', methods=['GET'])
+def primes(number):
+	return str(primenos(number))
 
 if __name__ == '__main__':
 	app.run(debug = True)
